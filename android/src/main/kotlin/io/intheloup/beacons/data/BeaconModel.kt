cd126cb5da2c073537b4b191f623f1c7fd614c53
Hexito <contact@hexito.com>
2019-04-19 02:17:29 +0300

    fix int string conversions

--- android/src/main/kotlin/io/intheloup/beacons/data/BeaconModel.kt
+++ android/src/main/kotlin/io/intheloup/beacons/data/BeaconModel.kt
@@ -6,13 +6,16 @@ package io.intheloup.beacons.data
 import org.altbeacon.beacon.Beacon
 
 class BeaconModel(
-        val ids: List<String>,
+        val ids: List<Any>,
         val distance: Double,
         val rssi: Int
 ) {
     companion object {
+        private fun intOrString(str: String) = str.toIntOrNull() ?: str
+
+
         fun parse(beacon: Beacon) = BeaconModel(
-                beacon.identifiers?.map { it.toString() } ?: emptyList(),
+                beacon.identifiers?.map { intOrString(it.toString()) } ?: emptyList(),
                 beacon.distance,
                 beacon.rssi
         )
